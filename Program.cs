13f58508c2c514ffe1d9d2867785c2fca4915a2d
Abubakr <thefox5000@gmail.com>
2024-04-29 17:02:56 +0300

    Create 3 tier layers

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+using ContactsBusinessLayer;
+
+namespace Contacts_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
